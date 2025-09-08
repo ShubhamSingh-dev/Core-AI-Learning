8891950d35032d00f74dccd292d91705470b8a83
Shubham Singh <mine.shubhamsingh@gmail.com>
2025-09-08 13:10:51 +0530

    learned streamlit

--- Python-Bootcamp/14-Streamlit/app.py
+++ Python-Bootcamp/14-Streamlit/app.py
@@ -5,25 +5,22 @@ import numpy as np
 ## Title of the aplication
 st.title("Hello Streamlit")
 
-## Diplay a Simple Text
-st.write("This is a imple text")
+## Display a Simple text
+st.write("This is a simple text")
 
-##create a simple Dataframe
+##Create a simple dataframe
 
 df = pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
 })
 
-
-## Display the Dataframe
-st.write("Here is the dataframe")
+## Display the dataframe
+st.write("Here is our dataframe")
 st.write(df)
 
-
-##create a line chart
-
-chart_data=pd.DataFrame(
-    np.random.randn(20,3),columns=['a','b','c']
+## create a line chart
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),columns=['a', 'b', 'c']
 )
 st.line_chart(chart_data)
